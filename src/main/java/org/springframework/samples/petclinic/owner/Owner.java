d15fcadc8972a6d9e821f9fae1470e109b5b60e3
thwin khant oo <thwinkhant338@gmail.com>
2026-09-09 02:26:13 +0630

    feat(ui): customize branding for Group-2 and loosen telephone validation

--- src/main/java/org/springframework/samples/petclinic/owner/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -58,7 +58,6 @@ public class Owner extends Person {
 
 	@Column
 	@NotBlank
-	@Pattern(regexp = "\\d{10}", message = "{telephone.invalid}")
 	private String telephone;
 
 	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
